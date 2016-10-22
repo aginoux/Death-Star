d8789b58ba5866e076037bceb242bcd25afa3c76
guillaumeBianchi <g.bianchi@hotmail.fr>
2016-10-22 16:28:22 +0200

    Premiere structure

---
+++ Scripts/Follow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = target.transform.position - transform.position;
+		transform.Translate(dir.normalized * 2 * Time.deltaTime, Space.World);
+	}
+}
