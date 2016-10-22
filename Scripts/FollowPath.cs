f5f182d46f0bfdbdbac885732fca0b3cb5a81cc1
guillaumeBianchi <g.bianchi@hotmail.fr>
2016-10-22 17:39:57 +0200

    Overview gameplay

---
+++ Scripts/FollowPath.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPath: MonoBehaviour {
+
+	public Transform target;
+	public float ship_speed = 2f;
+	Vector3 dir = new Vector3();
+
+	void Update () {
+		dir = target.transform.position - transform.position;
+		transform.Translate(dir.normalized * ship_speed * Time.deltaTime, Space.World);
+	}
+}
---
+++ Scripts/FollowWaypoint.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowWaypoint : MonoBehaviour {
+
+	private Transform target;
+
+	public float path_speed = 2f;
+	public float distance_path_nextwaypoint = 0.2f;
+	public bool finish = false;
+
+	private Vector3 dir = new Vector3();
+	private int waypoint_number = 0;
+
+	void Start(){
+		target = GetTransformChild.points [waypoint_number];
+	}
+
+	void Update () {
+		if (!finish) 
+		{
+			dir = target.transform.position - transform.position;
+			transform.Translate(dir.normalized * path_speed * Time.deltaTime, Space.World);
+		}
+
+		if (Vector3.Distance (transform.position, target.position) <= distance_path_nextwaypoint) 
+		{
+			//print (GetTransformChild.points.Length);
+			//print (waypoint_number);
+			if (waypoint_number == GetTransformChild.points.Length - 1) {
+				finish = true;
+			} 
+			else {
+				waypoint_number++;
+				target = GetTransformChild.points [waypoint_number];
+			}
+		}
+	}
+}
