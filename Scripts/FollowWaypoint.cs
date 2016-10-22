d413d1568ba36230a3db94561e7b54a37562762b
guillaumeBianchi <g.bianchi@hotmail.fr>
2016-10-22 18:06:51 +0200

    change speed (in progress)

--- Scripts/FollowWaypoint.cs
+++ Scripts/FollowWaypoint.cs
@@ -1,26 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class FollowWaypoint : MonoBehaviour {
 
 	private Transform target;
 
-	public float path_speed = 2f;
+	public float[] path_speed_array;
 	public float distance_path_nextwaypoint = 0.2f;
 	public bool finish = false;
 
 	private Vector3 dir = new Vector3();
 	private int waypoint_number = 0;
 
+	void Awake()
+	{
+		path_speed_array = new float[10];
+	}
+
 	void Start(){
 		target = GetTransformChild.points [waypoint_number];
+		path_speed_array [0] = 2f;
 	}
 
 	void Update () {
 		if (!finish) 
 		{
 			dir = target.transform.position - transform.position;
-			transform.Translate(dir.normalized * path_speed * Time.deltaTime, Space.World);
+			transform.Translate(dir.normalized * path_speed_array [waypoint_number] * Time.deltaTime, Space.World);
 		}
 
 		if (Vector3.Distance (transform.position, target.position) <= distance_path_nextwaypoint) 
